5aa64696219db03338b7d31c1ed37500932f6243
Didi-ua <andrdidyk@gmail.com>
2021-12-02 11:52:57 +0200

    changed history serializer

--- counter/views/serializers/history.py
+++ counter/views/serializers/history.py
@@ -3,7 +3,8 @@ from counter.models import History
 
 
 class HistorySerializer(serializers.ModelSerializer):
+    period = serializers.DateTimeField(format='%d.%m.%Y')
 
     class Meta:
         model = History
-        fields = '__all__'
+        fields = ['period', 'consumption', 'value', 'type']
