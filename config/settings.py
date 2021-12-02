14a6a26b3e9539c0f1637405ff12fdf27b83f134
Didi-ua <andrdidyk@gmail.com>
2021-12-02 11:43:22 +0200

    added filters for History

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'counter.apps.CounterConfig',
 ]
@@ -134,5 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ]
 }
--- counter/views/counters.py
+++ counter/views/counters.py
@@ -2,9 +2,20 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from django_filters.rest_framework.filters import DateFromToRangeFilter
+from django_filters import FilterSet
+
 from .serializers.counters import CounterSerializer
 from .serializers.history import HistorySerializer
 from counter.models import History
+
+
+class HistoryFilter(FilterSet):
+    period = DateFromToRangeFilter()
+
+    class Meta:
+        model = History
+        fields = ['period']
 
 
 class CounterViewSet(ModelViewSet):
@@ -15,6 +26,12 @@ class CounterViewSet(ModelViewSet):
 
     @action(detail=True, methods=['get'], serializer_class=HistorySerializer)
     def history(self, request, pk=None):
-        queryset = History.objects.filter(counter=self.get_object())
-        serializer = HistorySerializer(queryset, many=True)
+        queryset = History.objects.filter(
+            counter=self.get_object()
+        ).order_by('-period')
+        filtered_data = HistoryFilter(
+            data=request.query_params,
+            queryset=queryset
+        )
+        serializer = self.get_serializer(filtered_data.qs, many=True)
         return Response({"result": "success", "data": serializer.data})
